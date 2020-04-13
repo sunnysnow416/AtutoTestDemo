53821531baeafe13521d13e70819e48b6aedaa19
sunnysnow416 <570507051@qq.com>
2020-04-13 17:23:37 +0800

    第三次提交

---
+++ src/main/java/com/course/testng/BasicAnnotation.java
@@ -0,0 +1,12 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+	
+	@Test
+	public void testCase1(){
+		System.out.println("ÕâÊÇ²âÊÔÓÃÀı1¡£");
+	}
+
+}
