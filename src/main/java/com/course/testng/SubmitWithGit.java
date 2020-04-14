cf9423c5bf3c1779199875a521ccb92eea390294
sunnysnow416 <570507051@qq.com>
2020-04-14 15:59:39 +0800

    0414通过git命令上传文件测试

---
+++ src/main/java/com/course/testng/SubmitWithGit.java
@@ -0,0 +1,14 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class SubmitWithGit {
+	/*
+	 * 使用git窗口命令提交这个文件测试
+	 */
+	@Test
+	public void test(){
+		System.out.println("随便写点东西");
+	}
+
+}
